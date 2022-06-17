6e4442146ca7e532c0353e0a9b143694477b554a
brodieG <brodieG@users.noreply.github.com>
2022-06-17 05:55:20 -0400

    replace base_grp_eval with bsac

--- R/util.R
+++ R/util.R
@@ -15,23 +15,27 @@
 
 ## Used to be an S3 method, but that makes reloading package a problem.
 
-## Basic Split Apply Combine
-##
-## Evaluates quoted expressions in the context of data split by group.  Intended
-## purely for testing against C calculations.
-##
-## @param data a data frame with numeric columns
-## @param call quoted R call to evaluate
-## @param env environment to use as enclosure
-## @return numeric vector
+#' Basic Split Apply Combine
+#'
+#' Evaluates quoted expressions in the context of data split by group.  Intended
+#' purely for testing against r2c calculations.
+#'
+#' @export
+#' @param data a data frame with numeric columns
+#' @param call quoted R call to evaluate
+#' @param env environment to use as enclosure
+#' @return numeric vector
 
-base_grp_eval <- function(data, g, call, env=parent.frame()) {
-  dg <- split(data, g)
-  unlist(
-    unname(
-      lapply(dg, function(x, call) eval(call, envir=x, enclos=env), call=call)
-  ) )
+bsac <- function(call, g, data, MoreArgs=list(), env=parent.frame()) {
+  d.s <- split(data, g)
+  more <- list2env(MoreArgs, envir=env)
+  res <- lapply(
+    d.s,
+    function(x, call) eval(call, envir=x, enclos=more), call=call
+  )
+  unlist(res)
 }
+
 
 is.num_naked <- function(x)
   vapply(x, is.vector, TRUE, "numeric") |
