8d8e9eab1a32e67b6d1c3599ece1e8c1b9881514
brodieG <brodieG@users.noreply.github.com>
2022-07-29 13:50:57 -0400

    split C into inside/outside

--- extra/logo.R
+++ extra/logo.R
@@ -61,9 +61,11 @@ while(!all(picked)) {
   i <- i + 1
 }
 C.logo.tmp <- C.logo <- row.coords[picks,]
-plot(C.logo)
-points(C.logo[1,,drop=F], col='red')
-polypath(C.logo, col='yellow', border=NA)
+dropped <- logical(nrow(C.logo.tmp))
+kept <- seq_along(dropped)
+# plot(C.logo)
+# points(C.logo[1,,drop=F], col='red')
+# polypath(C.logo, col='yellow', border=NA)
 
 # Get rid of any points that do not deflect very much from the preceding or next
 # point.  In particular this is so we don't have anything in the connections
@@ -72,8 +74,11 @@ polypath(C.logo, col='yellow', border=NA)
 # Compute angle between 1st and 2nd and 1st and 3rd
 # Use angle to compute perpendicular from 1st to 3rd to 2nd.
 # Iteratively remove things.
+#
+# This is not entirely right, btw, but likely close enough it's okay
 
 repeat {
+  cat(nrow(C.logo.tmp), '')
   C.logo.2 <- C.logo.tmp[c(nrow(C.logo.tmp), seq_len(nrow(C.logo.tmp)), 1L),]
   v1 <- C.logo.2[-(nrow(C.logo.2) - 1:0),]
   v2 <- C.logo.2[-c(1, nrow(C.logo.2)),]
@@ -89,19 +94,25 @@ repeat {
     (sqrt(rowSums((v2 - v1) ^ 2)) * sqrt(rowSums((v3 - v1)^2)))
   )
   dist <- sin(ang0) * sqrt(rowSums(v21^2))
-  drop <- (is.na(dist) | dist < 1) & ang < .1
+  drop <- ((is.na(dist) | dist < 1) & ang < .1) |
+    (abs(sqrt(rowSums(v21^2))) <= sqrt(2) & sqrt(rowSums(v23^2)) <= sqrt(2))
+
   drop.w <- which(drop)
   if(!length(drop.w)) break
+  drop.w <- drop.w[c(TRUE, diff(drop.w) > 1)]
   # We do not want to drop sequential points
-  C.logo.tmp <- C.logo.tmp[-drop.w[c(TRUE, diff(drop.w) > 1)],,drop=FALSE]
+  C.logo.tmp <- C.logo.tmp[-drop.w,,drop=FALSE]
+  dropped[kept[drop.w]] <- TRUE
+  kept <- kept[-drop.w]
 }
 cat('\n')
 rng <- C.logo.tmp[,1] < 42
+rng <- C.logo.tmp[,2]>910 & C.logo.tmp[,1]>600
 cbind(C.logo.tmp, dist, ang, ang0, drop, v21, v23)[rng,]
 
 plot(C.logo.tmp[rng,])
-plot(C.logo.tmp)
-points(C.logo[drop, ], col='red')
+lines(C.logo.tmp[rng,])
+points(C.logo[dropped, ], col='red')
 
 
 # Now we have the coordinates, probably want to reduce to a similar number for
