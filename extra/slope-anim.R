d49572b6159aff8d23919f5266c2bc98b03310e2
brodieG <brodieG@users.noreply.github.com>
2023-01-07 09:13:07 -0500

    switch to matplot

--- extra/slope-anim.R
+++ extra/slope-anim.R
@@ -6,6 +6,7 @@ set.seed(1)
 n <- 500
 w <- 20
 pw <- w * 2
+offset <- -w/4
 x <- cumsum(c(0, runif(n - 1)))
 y <- cumsum(c(0, runif(n - 1) - .5))
 
@@ -32,7 +33,7 @@ w.vals <- roll_list(
 )
 w.vals2 <- roll_list(
   cbind,
-  x, y, pos=x, start=x.min, end=x.max, by=by, w=w * 2
+  x, y, pos=x, start=x.min + offset, end=x.max - offset, by=by, w=pw
 )
 
 d.centers <- vapply(w.vals, colMeans, numeric(2))
@@ -47,11 +48,13 @@ w.anchor <- seq(x.min, x.max, by)
 # * Show 2x more points than are fit on the window so we can see them on either
 #   side
 
-dev.off()
-dev.new(width=5, height=5)
+# dev.off()
+# dev.new(width=5, height=5)
 par(bg='white', mai=numeric(4))
-slope.x <- matrix(numeric(), nrow=3)
-slope.y <- matrix(numeric(), nrow=3)
+show.lines <- 50
+colors <- rainbow(show.lines)
+slope.x <- matrix(numeric(), nrow=2)
+slope.y <- matrix(numeric(), nrow=2)
 for(i in seq_along(w.anchor)) {
   plot.new()
   x.i <- w.vals[[i]][,1]
@@ -63,36 +66,31 @@ for(i in seq_along(w.anchor)) {
   if(is.na(y.u)) y.u <- 0
   if(is.na(x.u)) x.u <- 0
   slope.i <- slope(x.i, y.i)
-  x.plot <- w.anchor[i] + c(-w,w)
-  y.plot <- y.u + y.rng * c(-1,1)/2
+  x.plot <- w.anchor[i] + c(-w + offset, w + offset)
+  y.plot <- y.u + y.rng * c(-1, 1)/2
   plot.window(x.plot, y.plot)
-  slope.x <- cbind(c(w.anchor[i] + c(-w,w)/2, NA), slope.x)
+  slope.x <- cbind(
+    slope.x,
+    c(w.anchor[i] + c(-w,w)/2)
+  )
   slope.y <- cbind(
+    slope.y,
     c(
       y.u - (w/2 + (x.u - w.anchor[i])) * slope.i,
-      y.u + (w/2 - (x.u - w.anchor[i])) * slope.i,
-      NA
-    ),
-    slope.y
+      y.u + (w/2 - (x.u - w.anchor[i])) * slope.i
+    )
   )
-  show.lines <- 20
   in.frame <- slope.x[2,] > w.anchor[i] - w
-  in.frame <- in.frame & seq_along(in.frame) <= show.lines
+  in.frame <- in.frame & seq_along(in.frame) > length(in.frame) - show.lines
   slope.x <- slope.x[, in.frame, drop=FALSE]
   slope.y <- slope.y[, in.frame, drop=FALSE]
   line.n <- ncol(slope.x)
-  for(i in seq_len(line.n)) {
-    lines(
-      slope.x[, i, drop=TRUE], slope.y[, i, drop=TRUE],
-      col=grey(
-        ((show.lines - 1) - (i + (show.lines - line.n) - 1)) /
-        (show.lines - 1)
-      )
-    )
-  }
+  color.i <- seq(show.lines, show.lines - line.n + 1, by=-1)
+  if(!all(is.na(slope.y[1,])))
+    matlines(slope.x, slope.y, type='l', col=colors[color.i], lty=1)
   # if(!is.na(slope.i)) abline(w.anchor[i], slope.i)
   points(x.j, y.j, col='grey')
   points(x.i, y.i)
-  Sys.sleep(.05)
+  Sys.sleep(.0125)
 }
 
