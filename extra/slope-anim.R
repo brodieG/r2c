2d9b17bf177cd42b6edf5dc791569ae68831ec6a
brodieG <brodieG@users.noreply.github.com>
2023-01-08 09:35:30 -0500

    basic animation complete

--- extra/slope-anim.R
+++ extra/slope-anim.R
@@ -3,18 +3,22 @@ slope <- function(x, y)
 
 library(r2c)
 set.seed(2)
-n <- 150
+n <- 300
 w <- 20
 pw <- w * 2
 offset <- -w/4
 x <- cumsum(c(0, runif(n - 1)))
-y <- cumsum(c(0, runif(n - 1) - .5))
+y.raw <- runif(n - 1) - .5
+y <- cumsum(c(0, y.raw + c(0, y.raw[-1])))
+# y <- cumsum(c(0, rep(.5, n-1)))
 
 # The irony I haven't implemented yet what would allow me to do this easily.
 
-x.max <- ceiling(max(x)) + 2*w
-x.min <- -2*w
-by <- mean(diff(x)) / 4
+extra <- w
+x.max <- ceiling(max(x)) + pw + offset
+x.min <- floor(min(x)) + offset - w/2
+by <- mean(diff(x)) * 2
+show.lines <- 50
 
 roll_list <- function(fun, x, y, pos, start, end, by, w) {
   i <- seq(start, end, by)
@@ -27,10 +31,12 @@ y.rng <- max(
       function(x, y) if(length(y) < 2) 0 else diff(range(y)),
       x, y, pos=x, start=x.min, end=x.max, by=by, w=w * 2
 ) ) )
+# Points to compute the slope off
 w.vals <- roll_list(
   cbind,
   x, y, pos=x, start=x.min, end=x.max, by=by, w=w
 )
+# Background points
 w.vals2 <- roll_list(
   cbind,
   x, y, pos=x - offset, start=x.min, end=x.max, by=by, w=pw
@@ -50,9 +56,8 @@ w.anchor <- seq(x.min, x.max, by)
 
 # dev.off()
 # dev.new(width=5, height=5)
-par(bg='white', mai=numeric(4))
+# par(bg='white', mai=numeric(4))
 
-show.lines <- 50
 colors.raw <- rainbow(show.lines)
 # Desaturate colors as we get further by merging them on grey
 
@@ -65,44 +70,46 @@ colors <- rgb(
   ),
   maxColorValue=255
 )
+file.base <- '~/Downloads/r2c/slope-anim/img-%04d.png'
 {
   slope.x <- matrix(numeric(), nrow=2)
   slope.y <- matrix(numeric(), nrow=2)
+
   y.u <- 0
-  x.u <- 0
+  x.u <- w.anchor[1]
   for(i in seq_along(w.anchor)) {
+    cat(sprintf("Frame %04d\r", i))
+    png(sprintf(file.base, i), width=480, height=480)
+    par(mai=numeric(4))
     plot.new()
     y.u.prev <- y.u
     x.u.prev <- x.u
-    x.i <- w.vals[[i]][,1]
+    anc <- w.anchor[i]
+    x.i <- w.vals[[i]][,1] - anc
     y.i <- w.vals[[i]][,2]
-    x.j <- w.vals2[[i]][,1]
+    x.j <- w.vals2[[i]][,1] - anc
     y.j <- w.vals2[[i]][,2]
     y.u <- mean(y.i)
     x.u <- mean(x.i)
     if(is.na(y.u)) y.u <- y.u.prev
     if(is.na(x.u)) x.u <- x.u.prev
     slope.i <- slope(x.i, y.i)
-    x.plot <- w.anchor[i] + c(-w + offset, w + offset)
+
+    x.plot <- c(offset - w, offset + w)
     y.plot <- y.u + y.rng * c(-1, 1)/2
-    plot.window(x.plot, y.plot)
-    slope.x <- cbind(
-      slope.x,
-      c(w.anchor[i] + c(-w,w)/2)
-    )
+    plot.window(x.plot, y.plot, xaxs='i', yaxs='i')
+    rect(-w/2, y.plot[1], w/2, y.plot[2], col='#EEEEEE', border=NA)
+
+    slope.x <- cbind(slope.x - by, c(-w, w) / 2)
     slope.y <- cbind(
       slope.y,
-      c(
-        y.u - (w/2 + (x.u - w.anchor[i])) * slope.i,
-        y.u + (w/2 - (x.u - w.anchor[i])) * slope.i
-      )
+      c(y.u - (x.u + w/2) * slope.i,
+      y.u + (-x.u + w/2) * slope.i)
     )
-    in.frame <- slope.x[2,] > w.anchor[i] - w
-    in.frame <- in.frame & seq_along(in.frame) > length(in.frame) - show.lines
+    in.frame <- seq_len(ncol(slope.x)) > length(in.frame) - show.lines
     slope.x <- slope.x[, in.frame, drop=FALSE]
     slope.y <- slope.y[, in.frame, drop=FALSE]
     line.n <- ncol(slope.x)
-    rect(w.anchor[i] - w/2, -20, w.anchor[i] + w/2, 20, col='#EEEEEE', border=NA)
 
     if(!all(is.na(slope.y[1,])))
       matlines(
@@ -111,6 +118,9 @@ colors <- rgb(
     # if(!is.na(slope.i)) abline(w.anchor[i], slope.i)
     points(x.j, y.j, col='grey')
     points(x.i, y.i)
-    Sys.sleep(.0125)
+    dev.off()
   }
+  cat('\n')
 }
+# ffmpeg -pattern_type glob -i '*.png' -r 30 -pix_fmt yuv420p out.mp4
+
