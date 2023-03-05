ac76312552dc87a1ca54f4e01ddbd52351baee87
brodieG <brodieG@users.noreply.github.com>
2023-03-05 16:27:15 -0500

    more spotlight adjustments

--- extra/logo-3d-2.R
+++ extra/logo-3d-2.R
@@ -91,22 +91,30 @@ render(scene, lf=lf.start + c(3, 0, -3), la=la.start, out=out, fov=40, samples=2
 # bottom-top is -z -> +z
 # viewer-obj is +y -> -y
 
-R.xy2 <- R.xy
-R.xy2$x <- -R.xy2$x + mean(range(R.xy2$x))
 depth <- .1
+spacer <- .1
+two.xy <- with(objs.r, rbind(t.o, t.i))
+two.xy[, 1] <- -two.xy[, 1]
+two.xy[, 1] <- two.xy[, 1] - mean(range(two.xy[, 1]))
+
+R.xy2 <- cbind(-R.xy$x, R.xy$y)
+R.xy2[,1] <- R.xy2[,1] - min(R.xy2[,1]) + max(two.xy[,1]) + spacer
+
+c.xy <- with(objs.r, rbind(c.o, c.i))
+c.xy[,1] <- -c.xy[,1]
+c.xy[,1] <- c.xy[,1] - max(c.xy[,1])
+c.xy[,1] <- c.xy[,1] + min(two.xy[,1]) - spacer
+
 r3d <- extruded_polygon(
   R.xy2, top=depth, bottom=0,
   material=diffuse(color=R.color),
   holes=R.starts
 )
-scene <- rbind(
-  r3d,
-  xy_rect(xwidth=2, ywidth=2)
+two3d <- extruded_polygon(
+  two.xy, top=depth, bottom=0, material=diffuse(color=hoop.color)
 )
-render_preview(
-  width=400, height=400, samples=20, scene, fov=30,
-  lookfrom=c(0, 3, 2), lookat=c(0,0,0),
-  camera_up=c(0, 0, 1)
+c3d <- extruded_polygon(
+  c.xy, top=depth, bottom=0, material=diffuse(color=C.color)
 )
 
 light <- sphere(
@@ -116,55 +124,44 @@ light <- sphere(
     spotlight_width=30, spotlight_start_falloff=30
   )
 )
-scene <- rbind(
-  # obj,
-  r3d,
-  light,
-  studio,
-  NULL
-)
-out <- next_file(file.base)
 # render(scene, lf=lf.start, la=la.start, out=out, fov=40, samples=20)
-render_scene(
-  scene, lookfrom=lf.start, lookat=la.start, samples=20, fov=40,
-  width=400, height=400, camera_up=c(0, 0, 1)
-)
 
 # Mess with cone / spotlight
 
-spot.angle <- 10
-scene <- rbind(
-  xy_rect(xwidth=10, ywidth=10),
-  xz_rect(xwidth=10, zwidth=10, y=-.25),
-  r3d,
-  # cone(
-  #   radius=.125, material=diffuse("red"),
-  #   end=c(0, 0, .50)
-  #   # angle=c(90,0,0)
-  # ),
-  sphere(
-    x=.75, y=2, z=.75, radius=.05,
+width <- height <- 400
+samples <- 20
+light.x.pos <- c(mean(range(R.xy2[,1])), 0, mean(range(c.xy[,1]))) * 1.05
+for(focus.x in light.x.pos) {
+  spot.angle <- 16
+  light <- sphere(
+    x=0, y=1.25, z=.85, radius=.025,
     material=light(
-      spotlight_focus=c(0, 0, .25),
+      # spotlight_focus=c(.657, depth, .25),
+      spotlight_focus=c(focus.x, depth, .25),
       spotlight_width=spot.angle,
       spotlight_start_falloff=spot.angle,
-      intensity=5000, invisible=TRUE
+      intensity=4000, invisible=TRUE
     )
-  ),
-  sphere(x=2, y=2, z=2, material=light(intensity=1)),
-  # generate_studio(
-  #   distance=0, # elevation of base along the z-axis
-  #   # material=diffuse(color='yellow'),
-  #   # material=diffuse(checkercolor='green', checkerperiod=.5),
-  #   depth=-1,   # distance of backdrop along the y-axis
-  #   width=4, height=2, curvature=1
-  # ),
-  NULL
-)
-render_scene(
-  width=400, height=400, samples=20, scene, fov=30,
-  lookfrom=c(-2, 3, 1), lookat=c(0,0,.25),
-  camera_up=c(0, 0, 1)
-)
+  )
+  scene <- rbind(
+    xy_rect(xwidth=10, ywidth=20),
+    # xz_rect(xwidth=10, zwidth=10, y=-.5),
+    r3d,
+    two3d,
+    c3d,
+    light,
+    sphere(x=2, y=2, z=2, material=light(intensity=.5)),
+    NULL
+  )
+  out <- next_file(file.base)
+  render_scene(
+    filename=out,
+    width=width, height=height, samples=samples, scene, fov=35,
+    lookfrom=c(0, 2, 1) * 2, lookat=c(0,0,.35),
+    clamp_value=5,
+    camera_up=c(0, 0, 1)
+  )
+}
+
 
 
