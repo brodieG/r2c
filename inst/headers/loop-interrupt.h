17626b83166b1c9545ac98338a5724d672248abe
brodieG <brodieG@users.noreply.github.com>
2023-06-09 21:13:21 -0400

    copyright notices

--- inst/headers/loop-interrupt.h
+++ inst/headers/loop-interrupt.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) Brodie Gaslam
+ *
+ * This file is part of "r2c - Fast Iterated Statistic Computation in R"
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 or 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Go to <https://www.r-project.org/Licenses> for a copies of the licenses.
+ */
+
+// Loosely inspired by the R_ext/Itermacros.h from the GNU R sources
+
 // Assumed this is smaller than R_XLEN_T_MAX
 #define INTERRUPT_AT 10000000 /* keep synced with group.c */
 
