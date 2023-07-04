162eb8a19ca189b8c245eb5e9209426c1a77de16
brodieG <brodieG@users.noreply.github.com>
2023-07-04 10:55:17 -0400

    add loop code file

---
+++ R/code-loop.R
@@ -0,0 +1,24 @@
+## Copyright (C) Brodie Gaslam
+##
+## This file is part of "r2c - Fast Iterated Statistic Computation in R"
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 or 3 of the License.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## Go to <https://www.r-project.org/Licenses> for copies of the licenses.
+
+# - For ------------------------------------------------------------------------
+
+for_init <- function() NULL
+for_skip <- function() NULL
+for_n <- function() NULL
+r2c_for(zero, n) NULL
+
+
+
